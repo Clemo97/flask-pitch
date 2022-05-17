ab4f95b02b0c43205ce248cd2fc076c8485076f9
Clemo97 <lumumbaclement@gmail.com>
2022-05-17 09:53:54 +0300

    sql url

--- config.py
+++ config.py
@@ -4,17 +4,18 @@ class Config:
     '''
     General configuration parent class
     '''
-    SECRET_KEY = 'penyo'
-    #SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://clement:clemolumz@localhost/pitch1'
-    SQLALCHEMY_DATABASE_URI ='sqlite:///pitch1.db'
+    SECRET_KEY = 'clement'
+    #SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://clement:clemolumz@localhost/pitch1'
+    #SQLALCHEMY_DATABASE_URI ='sqlite:///pitch1.db'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    # email configurations
-    # MAIL_SERVER = 'smtp.googlemail.com'
-    # MAIL_PORT = 587
-    # MAIL_USE_TLS = True
-    # MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    # MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 class ProdConfig(Config):
     '''
@@ -34,8 +35,8 @@ class TestConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    #SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://clement:clemolumz@localhost/pitch1'
-    SQLALCHEMY_DATABASE_URI ='sqlite:///pitch1.db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://clement:clemolumz@localhost/pitch1'
+    #SQLALCHEMY_DATABASE_URI ='sqlite:///pitch1.db'
 
 class DevConfig(Config):
     '''
@@ -44,8 +45,8 @@ class DevConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    #SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://clement:clemolumz@localhost/pitch1'
-    SQLALCHEMY_DATABASE_URI ='sqlite:///pitch1.db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://clement:clemolumz@localhost/pitch1'
+    #SQLALCHEMY_DATABASE_URI ='sqlite:///pitch1.db'
 
     DEBUG = True
     ENV = 'development'
--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 export SECRET_KEY= 'clement'
 export MAIL_USERNAME='lumumbaclement@gmail.com'
-export MAIL_PASSWORD='34982353'
+export MAIL_PASSWORD='zcsxucviznzfflnx'
+DATABASE_URL = 'postgresql+psycopg2://clement:clemolumz@localhost/pitch1'
 
 python manage.py server
